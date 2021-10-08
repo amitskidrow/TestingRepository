b0abd7867047c2e1e2538c193cab3c277f1d3708
amitskidrow <32238330+amitskidrow@users.noreply.github.com>
2021-10-08 12:13:52 +0530

    first commit

---
+++ filemanager/src/main/java/com/xdm/filemanager/SampleAndroidLibMain.kt
@@ -0,0 +1,3 @@
+package com.xdm.filemanager
+
+class SampleFileManagerAndroidLib(val welcomeString: String = "Welcome to the sample implementation of an Android Library project published on GitHub Packages Registry")
